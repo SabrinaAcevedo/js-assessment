3df0782079affebdb3dbe9b4deaa371c9fe73c31
Sabrina Acevedo <112212824+SabrinaAcevedo@users.noreply.github.com>
2022-09-16 17:21:35 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+alert("My name is Sabrina Acevedo")
+alert("I am 22 years old")
+
+// Strings
+// console.log("Sabrina" + " Acevedo")
+// Variables
+let firstName = "Sabrina";
+let lastName = "Acevedo";
+
+// Number operations
+// console.log(11 + 11) // 22
+
+console.log("Im having a fun time in this class")
+
